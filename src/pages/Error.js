ac2d4ec36f1709b28722917fe8f3161aa9b3a7d5
J-Senpai <sianandarjason@gmail.com>
2023-07-27 15:12:23 +0700

    initial commit + navbar

---
+++ src/pages/Error.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Error() {
+  return (
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="text-center">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-3"> <span class="text-danger">Oops!</span> Halaman tidak ditemukan.</p>
+            <p class="lead">
+                Halaman yang kau cari tidak ada.
+            </p>
+            <Link to="/" className="btn btn-primary">Back to Dashboard</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Error
