5001640a35890041b9fdf35c8729dd2dab18502c
J-Senpai <sianandarjason@gmail.com>
2023-08-07 11:19:17 +0700

    added date range

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,11 +3,17 @@ import TrayekPie from '../components/TrayekPie'
 import AbsenPie from '../components/AbsenPie'
 import PendapatanGraph from '../components/PendapatanGraph'
 import KaryawanTable from '../components/KaryawanTable'
+import DateRangePickerComp from '../components/DateRange'
+
 
 function Dashboard() {
   return (
     <div className='dashboard-wrapper p-4'>
-        <h1>Dashboard</h1>
+        <div className='d-md-flex flex-row justify-content-between'>
+          <h1>Dashboard</h1>
+          <DateRangePickerComp />
+        </div>
+        
         <hr/>
         <div className='d-md-flex flex-row justify-content-around'>
             <TrayekPie />
